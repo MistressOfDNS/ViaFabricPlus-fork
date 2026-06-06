bddf1e60752810a38a6e187781790987448d74e3
Marie <104371913+MistressOfDNS@users.noreply.github.com>
2026-06-06 17:52:44 +0200

    Remove protocol screen VFP toggle

--- src/main/java/com/viaversion/viafabricplus/screen/impl/ProtocolSelectionScreen.java
+++ src/main/java/com/viaversion/viafabricplus/screen/impl/ProtocolSelectionScreen.java
@@ -25,7 +25,6 @@ import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
 import com.viaversion.viafabricplus.screen.VFPList;
 import com.viaversion.viafabricplus.screen.VFPListEntry;
 import com.viaversion.viafabricplus.screen.VFPScreen;
-import com.viaversion.viafabricplus.settings.impl.GeneralSettings;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import java.awt.*;
 import net.minecraft.client.Minecraft;
@@ -53,19 +52,10 @@ public final class ProtocolSelectionScreen extends VFPScreen {
             .pos(5, height - 25).size(98, 20).build());
         serverList.active = Minecraft.getInstance().getConnection() == null;
 
-        this.addRenderableWidget(Button.builder(getEnabledButtonText(), button -> {
-            GeneralSettings.INSTANCE.enableViaFabricPlus.setValue(!GeneralSettings.INSTANCE.enableViaFabricPlus.getValue());
-            this.rebuildWidgets();
-        }).pos(width / 2 - 60, height - 25).size(120, 20).build());
-
         this.addRenderableWidget(Button.builder(Component.translatable("report.viafabricplus.button"), button -> ReportIssuesScreen.INSTANCE.open(this))
             .pos(width - 98 - 5, height - 25).size(98, 20).build());
 
         super.init();
-    }
-
-    private static Component getEnabledButtonText() {
-        return GeneralSettings.INSTANCE.enableViaFabricPlus.getValue() ? Component.translatable("base.viafabricplus.enabled") : Component.translatable("base.viafabricplus.disabled");
     }
 
     public static class SlotList extends VFPList {
